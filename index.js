71f1280770dc6dcc0822af338b37299764cc2491
re-sha <163538829+re-sha@users.noreply.github.com>
2024-09-24 08:38:36 +0530

    add middleware for handling json requests

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { courseRouter } = require("./routes/course")
 const { adminRouter } = require("./routes/admin")
 const app = express()
 
+app.use(express.json())
+
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/course", courseRouter)
 app.use("/api/v1/admin", adminRouter)
