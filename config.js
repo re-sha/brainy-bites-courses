0ee880a6bc7abbadc330bc306ed07316843071c0
re-sha <163538829+re-sha@users.noreply.github.com>
2024-09-25 08:25:01 +0530

    minor changes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const JWT_ADMIN_PASSWORD = "43tr7ffq4weyio"
-const JWT_USER_PASSWORD = "wqaeyg23"
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
 
 module.exports = {
     JWT_ADMIN_PASSWORD,
